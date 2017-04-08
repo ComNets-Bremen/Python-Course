7fba53046b60233023d3323dc823b64a4b0acb4a
Jens Dede <jd@comnets.uni-bremen.de>
2017-04-08 13:47:57 +0200

    Be more detailled in the examples

--- examples/castingExample.py
+++ examples/castingExample.py
@@ -6,10 +6,13 @@ Convert between different types
 Jens Dede, 2017, jd@comnets.uni-bremen.de
 """
 
+# We use quotation marks so the numbers are actually strings: You cannot
+# perform mathematical operations on it
 floatString = "12.3"
 intString   = "45"
 
 print "Convert and add 100 to the number"
+# with float(<string>) or int(<string>) we convert the string back to a number
 print "Convert string", floatString, "to float:", float(floatString)+100
 print "Convert string", intString, "to float:", float(intString)+100
 print "Convert string", intString, "to int:", int(intString)+100
