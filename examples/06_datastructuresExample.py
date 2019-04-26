4a2f170ddc843c7c03e5e8c0441cffd3643614f9
Jens Dede <jd@comnets.uni-bremen.de>
2019-04-26 17:49:25 +0200

    py2->py3

--- examples/06_datastructuresExample.py
+++ examples/06_datastructuresExample.py
@@ -10,7 +10,7 @@ Jens Dede, 2019, jd@comnets.uni-bremen.de
 
 # Lists
 ###
-print 5*"#", "Lists"
+print(5*"#", "Lists")
 l = [1,2,3]          # List with three values
 
 # Append some items (numbers)
@@ -20,13 +20,13 @@ l.append(14)
 l.append(15)
 
 # Print complete list
-print "Complete list", l
+print("Complete list", l)
 
 # Print parts of the list
-print "First entry:", l[0]
-print "Last entry:", l[-1]
-print "Only till the 2nd entry:", l[:2]
-print "Only the last 2 entries:", l[-2:]
+print("First entry:", l[0])
+print("Last entry:", l[-1])
+print("Only till the 2nd entry:", l[:2])
+print("Only the last 2 entries:", l[-2:])
 
 # Change a list item
 l[1] = 123
@@ -34,26 +34,26 @@ l[1] = 123
 # Lists can be sorted
 l.sort()
 
-print "Changed list:", l
+print("Changed list:", l)
 
 # Iterate over the list
 for i in l:
-    print "List item", i
+    print("List item", i)
 
 # Tuples
 ###
 # \n creates a new line
-print "\n\n",5*"#", "Tuples"
+print("\n\n",5*"#", "Tuples")
 
 # Create a tuple
 t = (1,2,3,4,5)
 
 # Accessing tuple items
-print t, t[0]
+print(t, t[0])
 
 # Iterate over a tuple
 for i in t:
-    print "Tuple item", i
+    print("Tuple item", i)
 
 # Tuples cannot be changed after assigning them!
 # Advantage: They are faster than lists
@@ -62,22 +62,22 @@ for i in t:
 # Dictionaries (dict) (key-value storage)
 ###
 # \n creates a new line
-print "\n\n",5*"#", "Dictionaries (dict)"
+print("\n\n",5*"#", "Dictionaries (dict)")
 
 data = {}   # Empty dict
 data["name"] = "My Name"        # Assign value "My Name" using key "name" to dict data
 data["address"] = "My Address"
 
 # Access dict item using key
-print 'data["name"]:', data["name"]
+print('data["name"]:', data["name"])
 
 # Print complete dict
-print "data:", data
+print("data:", data)
 
 # Iterate over dict
 for key in data:
-    print "key:", key
-    print "data:", data[key]
+    print("key:", key)
+    print("data:", data[key])
 
 # Init a dictionary with values
 myDict = {
@@ -85,8 +85,8 @@ myDict = {
         "valueB": [1,2,3,4]
         }
 
-print myDict
-print myDict["valueA"]
+print(myDict)
+print(myDict["valueA"])
 
 # What to try out
 #################
